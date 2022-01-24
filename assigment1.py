365621de583fe7add6bf1489558a355ced66b09a
Adeola Adekoya <adeolaadekoya11@gmail.com>
2022-01-24 11:58:12 +0100

    solution to python weekly quiz

---
+++ assigment1.py
@@ -0,0 +1,36 @@
+# 1
+
+my_list = [ "The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog."]
+
+print(' '.join(my_list))
+
+# 2 indexing method
+
+new_list = ['this', "brown", 55, "oxen", True, 0.85]
+new_list[4] = False
+print(new_list)
+
+# 3  Write a Python program to print a specified list after removing the 0th, 4th and 5th elements. Sample List = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+
+# Expected Output = ['Green', 'White', 'Black']
+
+
+List = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+
+print(List [1:4])
+
+
+# 4  Write a program that takes in the user input of his favourite colour and adds it to an existing list of colours.
+
+
+color_list = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+
+colors_list = ['fusha', 'coral', 'orange']
+
+color_list.append(colors_list)
+
+print(color_list)
+
+
+
+
